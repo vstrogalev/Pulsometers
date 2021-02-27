2e2478b890a7d5ff1635664355ef6da6ad471ea0
Viacheslav <strogalev@hotmail.com>
2021-02-27 09:36:34 +0300

    Tabs are finished. Adaptive actualized

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
     $('.carousel__inner').slick({
       prevArrow: '<button type="button" class="slick-prev"><img src="icons/chevron-left-solid.png"></button>',
       nextArrow: '<button type="button" class="slick-next"><img src="icons/chevron-right-solid.png"></button>',
+      dotsClass: "my-dots",
       responsive: [
         {
           breakpoint: 992,
