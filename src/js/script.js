39d128469e7143229374f167f6682f144af5b953
Viacheslav <strogalev@hotmail.com>
2021-03-02 00:14:32 +0300

    Animations and validate ok

--- src/js/script.js
+++ src/js/script.js
@@ -115,10 +115,13 @@ $(document).ready(function(){
 	});
 
 
-	$("a[href^='#']").click(function(){
+	$("a[href='#up']").click(function(){
 		var _href = $(this).attr("href");
 		$("html, body").animate({scrollTop: $(_href).offset().top+"px"});
 		return false;
 	});
+
+	//Init wow.js
+	new WOW().init();
 
 });
