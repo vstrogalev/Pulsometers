55f645b06c64a513ee2e850ce5da5033e7da0a50
Viacheslav <strogalev@hotmail.com>
2021-03-01 21:26:09 +0300

    Scrolling up. Button and script

--- src/js/script.js
+++ src/js/script.js
@@ -103,6 +103,22 @@ $(document).ready(function(){
 				$('form').trigger('reset');
 		});
 		return false;
-});
+	});
+
+	// Scrolling up
+	$(window).scroll(function() {
+		if ($(this).scrollTop() > 1600) {
+			$('.pageUp').fadeIn();
+		} else {
+			$('.pageUp').fadeOut();
+		};
+	});
+
+
+	$("a[href^='#']").click(function(){
+		var _href = $(this).attr("href");
+		$("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+		return false;
+	});
 
 });
